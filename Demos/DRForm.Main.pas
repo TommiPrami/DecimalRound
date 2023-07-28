3a08a14691956d2d378ea4509487fc028549e64e
Tommi Prami <tommi.prami@gmail.com>
2023-07-28 11:03:45 +0300

    Minor update to test app

--- Demos/DRForm.Main.pas
+++ Demos/DRForm.Main.pas
@@ -27,10 +27,10 @@ implementation
 
 procedure TDRMainForm.ButtonTEstClick(Sender: TObject);
 begin
-  // 1.015 * 100 and rounded is different than 3.015 * 100
+  DoRound('2.245', 2.245);
+  DoRound('1.2451232323', 1.2451232323);
   DoRound('1.015 * 100', 1.015 * 100);
   DoRound('3.015 * 100', 3.015 * 100);
-  DoRound('2.245', 2.245);
 end;
 
 procedure TDRMainForm.DoRound(const AFormulaOrNumber: string; const ARawValue: Extended);
